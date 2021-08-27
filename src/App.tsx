33191db74bd72429c380d8855fed6bb013094a47
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-08-27 18:24:24 +0900

    making router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Aboutus from "./components/Aboutus";
+import Doctor from "./components/Doctor";
+import Home from "./components/home";
 
 function App() {
   return (
-    <div className="bg-black">
-      <h1 className="text-xl text-white">Hello tail</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/doctor" component={Doctor} />
+        <Route path="/Aboutus" component={Aboutus} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
